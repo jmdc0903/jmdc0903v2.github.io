0e711e5b9dbc15247d8d09b61c2a0b8c58f9248f
jmdc0903 <johnmichaeldelacruz0903@gmail.com>
2024-03-12 17:03:14 +0800

    loop activity

---
+++ javaActivities/assets/js/loopactivity.js
@@ -0,0 +1,17 @@
+//Loop Activity
+let Multiply = document.getElementById("btnMultiply");
+Multiply.addEventListener("click", function() {
+    let rowNumber = document.getElementById("rownumber").value;
+    let columnNumber = document.getElementById("columnnumber").value;
+    let Divs = "";
+    for(let x = 1; x <= rowNumber; x++) {
+        let cols = "";
+        for(let y = 1; y <= columnNumber; y++) {
+            cols += "<div class='col text-center m-1 border-bottom border-white ' style='width: fit-content; '>" + (x * y) + "</div>";
+            console.log(Divs)
+            console.log(x * y)
+        }
+         Divs += "<div class='row'>"+ cols +"</div>"
+    }
+    document.getElementById("divdata").innerHTML = Divs;
+});
