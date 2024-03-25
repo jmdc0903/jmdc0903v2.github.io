7c446d5f48c52185ab4cb21c420fcaa22f1d8ce6
jmdc0903 <johnmichaeldelacruz0903@gmail.com>
2024-03-25 19:54:55 +0800

    act2

---
+++ cc_activity/act2/assets/js/scripts.js
@@ -0,0 +1,14 @@
+
+
+
+let num1 = parseFloat(prompt("Please enter the first number:"));
+
+let num2 = parseFloat(prompt("Please enter the second number:"));
+
+let sum = num1 + num2;
+
+alert("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+
+
+
