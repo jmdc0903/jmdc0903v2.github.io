d785a7ce337a56f49b539a5ea2c3dba572992bc9
jmdc0903 <johnmichaeldelacruz0903@gmail.com>
2024-03-25 21:51:31 +0800

    act4

---
+++ cc_activity/act4/assets/js/scripts.js
@@ -0,0 +1,69 @@
+
+
+// let weight = parseFloat(prompt("Please enter your weight in kilograms:"));
+
+// let height = parseFloat(prompt("Please enter your height in meters:"));
+
+// let bmiResult = weight / (height * height);
+
+
+// let bmiCategory;
+
+// if (bmiResult < 16) {
+//     bmiCategory = "Severely Underweight";
+// } else if (bmiResult >= 16 && bmiResult <= 18.4) {
+//     bmiCategory = "Underweight";
+// } else if (bmiResult >= 18.5 && bmiResult <= 24.9) {
+//     bmiCategory = "Normal Weight";
+// } else if (bmiResult >= 25.0 && bmiResult <= 29.9) {
+//     bmiCategory = "Overweight";
+// } else if (bmiResult >= 30.0 && bmiResult <= 34.9) {
+//     bmiCategory = "Moderately Obese";
+// } else if (bmiResult >= 35.0 && bmiResult <= 39.9) {
+//     bmiCategory = "Severely Obese";
+// } else {
+//     bmiCategory = "Morbidly Obese";
+// }
+
+
+// console.log("BMI Result: " + bmiResult);
+// console.log(bmiCategory);
+
+
+
+// var Start = 0;
+// var End = 5;
+
+
+
+// for (var x = Start; x <= End; x++) {
+//     console.log(x);
+// }
+ 
+// let Min = 5;
+// let Max = 10;
+
+
+// Result:
+
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+
+
+let Min = 5;
+let Max = 10;
+
+for (var x = Min; x <= Max; x++)
+{
+    console.log(x);
+}
